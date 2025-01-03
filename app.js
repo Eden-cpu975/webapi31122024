8553ffaf3ee02d84134e3f8fb3aed8403f647cfe
Eden <Eden241218@gmail.com>
2025-01-03 21:45:45 +0200

    Add Support for Dot Env File

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+console.log(process.env.GOOGLE_USERS);
 const secure=require('./api/v1/middlewars/secure');
 app.use(secure);
-
 app.use('/product',productRouter);
 
 module.exports=app;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require ('dotenv').config();//הפעלת הפונקציה לטעינת הגדרות מהקובץ
 const http=require('http');
 const app=require('./app');
 const port=5050;
@@ -5,3 +6,4 @@ const srv=http.createServer(app);
 srv.listen(port,()=>{
  console.log("server is up");
 });
+//comment
