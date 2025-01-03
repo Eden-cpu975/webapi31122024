99eccc8e6a52bd0c3567b550265504f335046985
Eden <Eden241218@gmail.com>
2025-01-03 20:55:43 +0200

    Add Seacurity to app.js

--- app.js
+++ app.js
@@ -6,18 +6,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const secure=(res,req,next)=>{};
-app.use((req,res,next)=>{
-    let arrIps=["192.168.15","8.6.5.9","23.54.23.1"];
-for(let i=0;i<arrIps.length;i++)
-{
-    if(req.ip==arrIps[i])
-    {
-        return next();
-    }
-}
-return res.status(401).json({Msg:"Not Authorized"});
-});
+const secure=require('./api/v1/middlewars/secure');
+app.use(secure);
 
 app.use('/product',productRouter);
 
